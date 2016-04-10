26bf0f354157db302ac51af8e0a053f582475d24
Hauke Rehfeld <hauke@haukerehfeld.de>
2016-04-11 00:58:09 +0200

    cln

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,13 +1,18 @@
 (function () {
+	THREE.Vector2.multiplyScalar = function(x) { this.x *= x; this.y *= x; }
+	THREE.Vector2.multiply = function(v) { this.x *= v.x; this.y *= v.y; }
+	THREE.Vector2.divideScalar = function(x) { this.x /= x; this.y /= x; }
+	THREE.Vector2.divide = function(v) { this.x /= v.x; this.y /= v.y; }
 	var radians = function(angle) { return angle * Math.PI / 180; };
 
-	var mousePosition = new THREE.Vector2(0, 0);
-	var initMouseControls = function() {
+	var Input = function() {
+		this.screenPosition = new THREE.Vector2(0, 0);
+		var self = this;
 		var onMouseMove = function(event) {
-			mousePosition.x = event.x;
-			mousePosition.y = event.y;
+			self.screenPosition.x = event.x;
+			self.screenPosition.y = event.y;
 		};
-
+		
 		this.dispose = function() {
 			document.removeEventListener( 'mousemove', onMouseMove, false );
 		}
@@ -16,16 +21,17 @@
 
 	var windowSize = new THREE.Vector2(window.innerWidth, window.innerHeight);
 	var fovv = 70;
-	var mousePlaneDepth = 1;
+	var mousePlaneDepth = 10;
 
     var camera, scene, renderer;
 	var mesh;
 	var point;
-	initMouseControls();
+	var material = new THREE.LineBasicMaterial( { color : 0xffdd00, linewidth: 4 } );
+	var input = new Input();
     init();
     animate();
 	function init() {
-        camera = new THREE.PerspectiveCamera(fovv, windowSize.x / windowSize.y, 1, 1000);
+        camera = new THREE.PerspectiveCamera(fovv, windowSize.x / windowSize.y, 0.01, 1000);
         camera.position.x = 0;
         camera.position.y = 0;
 		camera.position.z = 0;
@@ -33,7 +39,6 @@
         scene = new THREE.Scene();
         var texture = new THREE.TextureLoader().load('../assets/textures/crate.gif');
         var geometry = new THREE.BoxBufferGeometry(200, 200, 200);
-        var material = new THREE.MeshBasicMaterial({map: texture});
         mesh = new THREE.Mesh(geometry, material);
 		//scene.add(mesh);
 
@@ -52,45 +57,60 @@
         renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
+	function screenToNdc(v) {
+		var ndc = new THREE.Vector2().copy(v);
+	    ndc.x *= 2;
+		ndc.y *= 2;
+	    ndc.x -= 1;
+	    ndc.y -= 1;
+		ndc.y *= -1;
+		return ndc;
+	};
+
     function animate() {
         requestAnimationFrame(animate);
-        mesh.rotation.x += 0.005;
-	    mesh.rotation.y += 0.01;
 
-	    var relMousePos = new THREE.Vector3(
-		    mousePosition.x / windowSize.x
-		    , mousePosition.y / windowSize.y
-		    , 0
-	    );
-
-	    relMousePos.x *= 2;
-	    relMousePos.y *= 2;
-
-	    relMousePos.x -= 1;
-	    relMousePos.y -= 1;
-
-	    relMousePos.y *= -1;
+	    var relScreenPos = new THREE.Vector2().copy(input.screenPosition).divide(windowSize);
+	    relScreenPos = screenToNdc(relScreenPos);
 
 	    var fovh = fovv * camera.aspect;
-
-
-	    var planePos = (relMousePos.multiply(
-		    new THREE.Vector3(
-			    Math.tan(radians(fovh/2))
+	    var planePos = (relScreenPos.multiply(
+		    new THREE.Vector2(
+			      Math.tan(radians(fovh/2))
 			    , Math.tan(radians(fovv/2))
-			    , 0
-		    )
+		    ).multiplyScalar(mousePlaneDepth)
 	    ));
+	    planePos = new THREE.Vector3(planePos.x, planePos.y, -mousePlaneDepth)
 	    planePos.add(camera.position);
-	    planePos.z -= mousePlaneDepth;
-
-	    console.log(planePos);
 
 	    if (point) {
 		    scene.remove(point);
 	    }
-	    point = new THREE.BoxHelper(new THREE.ArrowHelper(new THREE.Vector3(0.1, 0.2, 1), planePos, 1, 0xffff00));
+	    point = new THREE.ArrowHelper(new THREE.Vector3(0, 1, 0), planePos, 1, 0xffff00);
 	    scene.add(point);
+
+	    var curve = new THREE.QuadraticBezierCurve3(
+		    new THREE.Vector3(0, -1, 0).add(camera.position),
+		    new THREE.Vector3(0, -1, -mousePlaneDepth).add(camera.position),
+		    planePos
+	    );
+
+
+	    if (mesh) {
+		    for (var i = 0; i < mesh.length; ++i) {
+			    scene.remove(mesh[i]);
+		    }
+	    }
+
+	    var geometry = new THREE.Geometry();
+	    geometry.vertices = curve.getPoints( 50 );
+	    //geometry.vertices = [new THREE.Vector3(0, -1, 0).add(camera.position), planePos];
+	    var o = new THREE.Line( geometry, material );
+	    mesh = [];
+	    mesh.push(o);
+	    for (var i = 0; i < mesh.length; ++i) {
+		    scene.add(o);
+	    }
 
 
         renderer.render(scene, camera);
